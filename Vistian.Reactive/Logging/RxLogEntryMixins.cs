964ad3a35cef2c27abf76ff38375901e4bcf32f8
jcmm33 <jamie.mutton@vistian.co.uk>
2016-11-11 13:09:31 +0000

    Added in missing extension file, RxLogEntryMixins.

---
+++ Vistian.Reactive/Logging/RxLogEntryMixins.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Vistian.Contract;
+using Vistian.Xamarin;
+
+namespace Vistian.Reactive.Logging
+{
+    /// <summary>
+    /// Extensions enabling logging from within classes.
+    /// </summary>
+    /// 
+    [Preserve(AllMembers = true)]
+    public static class RxLogEntryMixins
+    {
+        public static RxLogEntry Log(this object publisher, object instance, [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int lineNo = 0)
+        {
+            Guard.NotNull(publisher);
+
+            var meta = RxLog.CreateMeta(publisher.GetType(), callerMemberName, lineNo);
+            var entry = new RxLogEntry(meta, instance);
+
+            RxLog.Log(entry);
+
+            return entry;
+        }
+
+
+        public static RxLogEntry<T> Log<T>(this object publisher, T instance, [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int lineNo = 0)
+        {
+            Guard.NotNull(publisher);
+            Guard.NotNull(instance);
+
+            var meta = RxLog.CreateMeta(publisher.GetType(), callerMemberName, lineNo);
+            var entry = new RxLogEntry<T>(meta, instance);
+
+            RxLog.Log(entry);
+
+            return entry;
+        }
+
+    }
+}

cf8a4af12f6f444d8009aa9232665a6f163644fb
jcmm33 <jamie.mutton@vistian.co.uk>
2016-11-17 12:06:32 +0000

    Added throw extension for classified logged items.

---
+++ Vistian.Reactive/Logging/Providers/Classified/ThrowExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vistian.Reactive.Logging.Providers
+{
+    public static class ThrowExtensions
+    {
+        /// <summary>
+        /// Simple extension to force a throw once logged.
+        /// </summary>
+        /// <param name="entry"></param>
+        /// <param name="exception"></param>
+        public static void Throw(this RxLogEntry<Classified> entry,
+            Func<RxLogEntry<Classified>, Exception> exception)
+        {
+            throw exception(entry);
+        }
+    }
+}

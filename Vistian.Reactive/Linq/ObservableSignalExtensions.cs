4a9c17fec66ddafe07bb18b5f08e958669dc2f0e
jcmm33 <jamie.mutton@vistian.co.uk>
2017-04-04 15:23:50 +0100

    Added Signal extension

---
+++ Vistian.Reactive/Linq/ObservableSignalExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vistian.Reactive.Linq
+{
+    public static class ObservableSignalExtensions
+    {
+        /// <summary>
+        /// Utility extension to turn any observable stream into one of <see cref="Unit"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IObservable<Unit> Signal<T>(this IObservable<T> source)
+        {
+            return source.Select(_ => Unit.Default);
+        }         
+    }
+}
